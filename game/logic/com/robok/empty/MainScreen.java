ee13282c52cafaa9fa9cb32679bc86d77bc4919e
trindadedev13 <devsuay@gmail.com>
2024-09-23 07:22:12 +0000

    initial commit

---
+++ game/logic/com/robok/empty/MainScreen.java
@@ -0,0 +1,11 @@
+package com.robok.empty;
+
+import robok.game.screen.GameScreen;
+
+public class MainScreen extends GameScreen {
+
+    @Override
+    public void onScreenCreated() {
+        
+    }
+}
